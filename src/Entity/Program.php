991e71f03f07fa8e4ccbe67dc3580b39cf6c183a
BrunoLagasse <brlagasse@laposte.net>
2020-11-30 12:20:31 +0100

    quest 6

---
+++ src/Entity/Program.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProgramRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ */
+class Program
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $program;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProgram(): ?string
+    {
+        return $this->program;
+    }
+
+    public function setProgram(string $program): self
+    {
+        $this->program = $program;
+
+        return $this;
+    }
+}
