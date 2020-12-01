5a270815b4d03b049ec9352ef0038446a42fee1a
BrunoLagasse <68063824+BrunoLagasse@users.noreply.github.com>
2020-12-01 16:39:58 +0100

    Update ProgramController.php

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,20 +1,30 @@
 <?php
-// src/Controller/ProgramController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/programs", name="program_")
+*/
+
 class ProgramController extends AbstractController
 {
-/**
- * @Route("/programs/", name="program_index")
- */
-public function index(): Response
-{
-    return $this->render('program/index.html.twig', [
-       'index' => 'Wild Séries',
-    ]);
-}
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig');
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+    }
 }
